55e8158e811534de48f3bc8e21f40a9a81d3f5b0
Pigycoin Core Developer <61416421+pigycoin-project@users.noreply.github.com>
2020-02-25 01:39:35 +0000

    Update guiconstants.h

--- src/qt/guiconstants.h
+++ src/qt/guiconstants.h
@@ -49,7 +49,7 @@ static const int MAX_URI_LENGTH = 255;
 #define SPINNER_FRAMES 36
 
 #define QAPP_ORG_NAME "pigycoin"
-#define QAPP_ORG_DOMAIN "faithco.in"
+#define QAPP_ORG_DOMAIN "pigycoin.org"
 #define QAPP_APP_NAME_DEFAULT "pigycoin-Qt"
 #define QAPP_APP_NAME_TESTNET "pigycoin-Qt-testnet"
 
